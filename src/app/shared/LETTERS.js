19cf8dd011e384ed962629f6dafec5ad109277b0
Ryan Girard <rmg9923@gmail.com>
2026-01-04 23:59:32 -0500

    Finished alphabet objects array

--- src/app/shared/LETTERS.js
+++ src/app/shared/LETTERS.js
@@ -1,7 +1,29 @@
 import A from '../assets/images/a.png';
 import B from '../assets/images/b.png';
 import C from '../assets/images/c.png';
-import D from '../assets/images/d.png'
+import D from '../assets/images/d.png';
+import E from '../assets/images/e.png';
+import F from '../assets/images/f.png';
+import G from '../assets/images/g.png';
+import H from '../assets/images/h.png';
+import I from '../assets/images/i.png';
+import J from '../assets/images/j.png';
+import K from '../assets/images/k.png';
+import L from '../assets/images/l.png';
+import M from '../assets/images/m.png';
+import N from '../assets/images/n.png';
+import O from '../assets/images/o.png';
+import P from '../assets/images/p.png';
+import Q from '../assets/images/q.png';
+import R from '../assets/images/r.png';
+import S from '../assets/images/s.png';
+import T from '../assets/images/t.png';
+import U from '../assets/images/u.png';
+import V from '../assets/images/v.png';
+import W from '../assets/images/w.png';
+import X from '../assets/images/x.png';
+import Y from '../assets/images/y.png';
+import Z from '../assets/images/z.png';
 
 export const LETTERS = [
     {
@@ -27,89 +49,111 @@ export const LETTERS = [
     {
         id: 5,
         name: 'E',
+        src: E,
     },
     {
         id: 6,
         name: 'F',
+        src: F,
     },
     {
         id: 7,
         name: 'G',
+        src: G,
     },
     {
         id: 8,
         name: 'H',
+        src: H,
     },
     {
         id: 9,
         name: 'I',
+        src: I,
     },
     {
         id: 10,
         name: 'J',
+        src: J,
     },
     {
         id: 11,
         name: 'K',
+        src: K,
     },
     {
         id: 12,
         name: 'L',
+        src: L,
     },
     {
         id: 13,
         name: 'M',
+        src: M,
     },
     {
         id: 14,
         name: 'N',
+        src: N,
     },
     {
         id: 15,
         name: 'O',
+        src: O,
     },
     {
         id: 16,
         name: 'P',
+        src: P,
     },
     {
         id: 17,
         name: 'Q',
+        src: Q,
     },
     {
         id: 18,
         name: 'R',
+        src: R,
     },
     {
         id: 19,
         name: 'S',
+        src: S,
     },
     {
         id: 20,
         name: 'T',
+        src: T,
     },
     {
         id: 21,
         name: 'U',
+        src: U,
     },
     {
         id: 23,
         name: 'V',
+        src: V,
     },
     {
         id: 23,
         name: 'W',
+        src: W,
     },
     {
         id: 24,
         name: 'X',
+        src: X,
     },
     {
         id: 25,
         name: 'Y',
+        src: Y,
     },
     {
         id: 26,
         name: 'Z',
+        src: Z,
     },
 ]
