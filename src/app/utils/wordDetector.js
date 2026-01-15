c92c060c54b5643653068b849ebf4d21300c6357
ShmeakShmcok <shmocktography@gmail.com>
2026-01-14 23:03:15 -0800

    API word check

---
+++ src/app/utils/wordDetector.js
@@ -0,0 +1,18 @@
+async function checkIfWord(line) {
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${line}`);
+    
+    if (response.ok) {
+       await response.json();
+      console.log(`"${line}" is a valid word!`);
+      return true;
+    } else {
+      console.log(`"${line}" is not a valid word`);
+      return false;
+    }
+  } catch (error) {
+    console.log('Error checking word:', error);
+    return false;
+  }
+}
+export default checkIfWord
